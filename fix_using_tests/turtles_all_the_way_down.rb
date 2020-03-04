58d2fe270afe7024cb260dd3eacf90c612285b1e
Josh Allen <joshmrallen@gmail.com>
2020-03-04 16:45:46 +0000

    Done.

--- fix_using_tests/turtles_all_the_way_down.rb
+++ fix_using_tests/turtles_all_the_way_down.rb
@@ -2,16 +2,25 @@
 def turtles
     [
       {name: "Donatello", weapon: "bo-staff", traits: ["serious", "leader"]},
-      {name: "Michaelangelo", weapon: "nunchuks", traits: "party dude"},
+      {name: "Michaelangelo", weapon: "nunchuks", traits: ["party dude"]},
       {name: "Raphael", weapon: "sais", traits: ["cool", "rude"]},
       {name: "Leonardo", weapon: "katanas", traits: ["loves being a turtle"]}
     ]
 end
 
 def turtle_traits(turtles)
+  
+  traits = []
+  
   turtles.map do |turtle|
-    turtle[:traits].each do |trait|
-      trait
+    turtle.each do |key, value| 
+      if key == :traits
+        traits.push(value)
+      end
     end
   end
+
+
+  return traits
+  
 end
